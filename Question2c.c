01aa0f8a9603c0485a170dba6e187de76f5e3037
ZariffZullastree <156812431+ZariffZullastree@users.noreply.github.com>
2024-01-16 23:39:58 +0800

    Question2c.c

---
+++ Question2c.c
@@ -0,0 +1,40 @@
+// Client Side
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define PORT 8080
+#define SERVER_IP "127.0.0.1"
+
+int main() {
+    int client_socket;
+    struct sockaddr_in server_addr;
+
+    if ((client_socket = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = inet_addr(SERVER_IP);
+    server_addr.sin_port = htons(PORT);
+
+    if (connect(client_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
+        perror("Connection failed");
+        exit(EXIT_FAILURE);
+    }
+
+ 
+    int random_number;
+    recv(client_socket, &random_number, sizeof(random_number), 0);
+
+    printf("Received random number from server: %d\n", random_number);
+
+    // Close the socket
+    close(client_socket);
+
+    return 0;
+}
